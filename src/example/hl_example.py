4df2b45cf6b008bf326f3735dedd5b257aac5fa6
Rainer Poisel <rainer@honeytreelabs.com>
2025-03-02 21:24:23 +0100

    refactor: syntactic sugar for the example lib wrapper

--- src/example/hl_example.py
+++ src/example/hl_example.py
@@ -1,17 +1,31 @@
 from collections.abc import Generator
 from contextlib import contextmanager
+from dataclasses import dataclass
 
 import example
 
 
+@dataclass
+class ExampleHandler:
+    _h: example.Handler
+
+    @property
+    def something(self) -> int:
+        return example.get_something(self._h)
+
+    @something.setter
+    def something(self, value: int) -> None:
+        example.set_something(self._h, value)
+
+
 @contextmanager
-def example_handler() -> Generator[example.Handler, None, None]:
-    h = None
+def example_handler() -> Generator[ExampleHandler, None, None]:
+    h: example.Handler | None = None
     try:
         h = example.alloc_something()
         if h is None:
             raise RuntimeError("Could allocate something.")
-        yield h
+        yield ExampleHandler(h)
     finally:
         if h is None:
             return
--- tests/test_swig_example.py
+++ tests/test_swig_example.py
@@ -1,9 +1,8 @@
-from example import get_something, set_something
 from hl_example import example_handler
 
 
 def test_swig_example() -> None:
     with example_handler() as h:
-        assert get_something(h) == 23
-        set_something(h, 42)
-        assert get_something(h) == 42
+        assert h.something == 23
+        h.something = 42
+        assert h.something == 42
